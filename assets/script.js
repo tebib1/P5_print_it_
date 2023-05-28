1ce1bdf0addd507a658fbc26516a7f05e8b9a79e
tebib nourhene <rebainourhene1@gmail.com>
2023-05-28 12:01:19 +0200

    first commit

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const slides = [
+
+	{
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+const baseUrl = "./assets/images/slideshow/";
+const precedent = document.querySelector('.arrow_left');
+const suivant = document.querySelector('.arrow_right');
+const image = document.querySelector('.banner-img')
+let positionslide = 0;
+
+precedent.addEventListener("click", function () {
+	if (positionslide == 0) {
+		positionslide = slides.length - 1;
+	} else { positionslide-- }
+	image.src = baseUrl + slides[positionslide].image;
+});
+
+suivant.addEventListener("click", function () {
+	if (positionslide == slides.length -1 ) {
+		positionslide = 0;
+	} else { positionslide++ }
+	image.src = baseUrl + slides[positionslide].image;
+});
